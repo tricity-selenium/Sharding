d7de7ea06adbc86c2fa916bc87c04b7c21ac6cad
tricity-selenium <tricity.selenium@gmail.com>
2026-09-24 10:27:29 +0530

    Test3 Changes

--- tests/test3.spec.ts
+++ tests/test3.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('Test3', async ({ page }) => {
-  await page.goto('https://testautomationpractice.blogspot.com/');
+  await page.goto('https://www.testmuai.com/selenium-playground/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Automation/);
+  await expect(page).toHaveTitle(/Selenium/);
 });
 
 
